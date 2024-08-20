1a91df9ee96ed033f16459e2381c844db75f34a1
SenyorJoaquin <joaquin.tolentino@ciit.edu.ph>
2024-08-21 03:28:22 +0800

    Added a Dark Mode toggle

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+const toggleSwitch = document.getElementById('darkmode-toggle');
+const themeLink = document.getElementById('theme-link');
+const githubIcon = document.getElementById('github-icon');
+
+
+function switchTheme() {
+    if (toggleSwitch.checked) {
+        themeLink.href='index-dark.css';
+        githubIcon.src = './assets/github_logo_inverted.png'; 
+    } else {
+        themeLink.href='index.css';
+        githubIcon.src = './assets/github_logo.png';  
+    } 
+    
+}
+
+toggleSwitch.addEventListener('change', switchTheme);
+
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
